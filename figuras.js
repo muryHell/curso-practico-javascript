f4681203141d717205389f52471f3ca6bf056ba4
Amauri <mury117@gmail.com>
2022-01-13 16:25:46 -0600

    despues de la clase

--- figuras.js
+++ figuras.js
@@ -1,1 +1,62 @@
-console.log("Gola xd");
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Lados del cuadrado miden " + ladoCuadrado);
+
+const perimetroC = ladoCuadrado *4;
+
+console.log("Perimetro " + perimetroC + "cm");
+
+const areaC = ladoCuadrado * ladoCuadrado;
+
+console.log("area " + areaC + "cm2");
+console.groupEnd();
+
+//triangulo
+console.group("triangulo");
+const ladoT1 = 6;
+const ladoT2 = 6;
+const baseT3 = 4;
+
+console.log("Los lados del triangulo miden "
++ladoT1 + "cm, "
++ladoT2 + "cm, "
++baseT3 + "cm"
+);
+
+const alturaT = 5.5;
+
+console.log("La altura es " + alturaT + "cm");
+
+const perimetroT = ladoT1+ladoT2+baseT3;
+
+console.log("El perimetro es " + perimetroT + " cm");
+
+const areaT = baseT3*alturaT/2;
+
+console.log("El area es " + areaT + " cm2");
+
+console.groupEnd();
+
+//circulo
+console.group("circulo");
+
+//radio
+const radioC = 4;
+//diametro
+const diametroC = radioC *2;
+//pi
+const pi = Math.PI;
+//circunferencia
+const perimetroCirc = diametroC * pi;
+//area
+const areaCirc = pi* (radioC*radioC);
+
+console.log("El radio del ciruclu es " + radioC + " cm");
+
+console.log("El diametro del circulo es " + diametroC + " cm");
+
+console.log("El perimetro del circulo es " + perimetroCirc + " cm");
+
+console.log("El area del circulo es "+ areaCirc + " cm2");
+
+console.groupEnd();
