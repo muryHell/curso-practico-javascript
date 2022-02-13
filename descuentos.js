e56d9a7dcd1ee0003820f42fa76ea0400b9f715b
Amauri <mury117@gmail.com>
2022-02-12 23:25:07 -0600

    talerrr

--- descuentos.js
+++ descuentos.js
@@ -11,6 +11,20 @@
 function calculaDescuento(precio,descuento) {
   const procentajeDescuento = 100 - descuento;
   const precioDescuento = (precio * procentajeDescuento)/100;
-  
+
   return precioDescuento;
+}
+
+function priceDiscount(){
+  const inputPrecio = document.getElementById('inputPrecio');
+  const priceValue = inputPrecio.value;
+
+  const inputDescuento = document.getElementById('inputDescuento');
+  const descuentoValue = inputDescuento.value;
+
+  const pricioDescuento = calculaDescuento(priceValue,descuentoValue);
+
+  const resultP = document.getElementById('resultP');
+
+  resultP.innerText = "El precio con descuento es $" + pricioDescuento;
 }
